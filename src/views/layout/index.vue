fdade403da94f2c9bba836fa26e0bda0adef46cc
unknown <759036585@qq.com>
2020-01-18 16:26:32 +0800

    首页基本布局完成

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,16 +1,60 @@
 <template>
 <!--    布局组件-->
-  <div>
-    <router-view></router-view>
+  <div class="container">
+    <van-nav-bar
+            title="今日看点"
+            right-text="搜索"
+            fixed
+            @click-right="onClickRight"
+    />
+    <div class="my-wrapper" :class="{ noTop: !showNavBar}">
+      <router-view></router-view>
+    </div>
+    <van-tabbar>
+      <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="chat-o" to="/question">问答</van-tabbar-item>
+      <van-tabbar-item icon="video-o" to="/video">视频</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" to="/user">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  name: 'index',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    showNavBar () {
+      return this.$route.path !== '/user'
+    }
+  },
+  methods: {
+    onClickRight () {
+      this.$router.push('/search')
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .container {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    .my-wrapper {
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      padding-top: 46px;
+      padding-bottom: 50px;
+      box-sizing: border-box;
+      &.noTop {
+        padding-top: 0;
+      }
+    }
+  }
 
 </style>
