b78335f043d4e5f7c205a6e0081b620723c5bb35
unknown <759036585@qq.com>
2020-01-19 13:50:42 +0800

    处理token失效问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import JSONBig from 'json-bigint'
 import store from '../store/index'
+import router from '../router/index'
 
 // 创建一个axios实例 和原来的axios没有关系
 const instance = axios.create({
@@ -29,8 +30,35 @@ instance.interceptors.response.use(function (response) {
   } catch (error) {
     return response.data
   }
-}, function (error) {
+}, async function (error) {
+  let toPath = { path: '/login', query: { redirectUrl: router.currentRoute.path } } // 跳转对象
   // 错误的时候token容易失效
+  if (error.response && error.response.status === 401) {
+    try {
+      // 表示token过期 先判断 是否有refresh_token
+      // 这里不应该再用instance 因为instance 会再次进入拦截器 用默认的axios
+      let result = await axios({
+        method: 'put',
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${store.state.user.token}`
+        }
+      })
+      store.commit('updateUser', {
+        user: {
+          token: result.data.data.token, // 拿到token之后
+          refresh_token: store.state.user.refresh_token
+        }
+      })
+      return instance(error.config) // 把刚才错误的请求再次发送出去 然后将promise返回
+    } catch (error) {
+      // 如果错误 表示补救措施也没用了 应该跳转到登录页 并且 把废掉的user全部干掉
+      store.commit('clearUser') // 所有的用户信息清空
+      router.push(toPath) // 跳回到登录页
+    }
+  } else {
+    router.push(toPath)
+  }
   return Promise.reject(error)
 })
 
