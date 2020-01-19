76daba45fcf603baf2a565840a4b09286f04e8cb
unknown <759036585@qq.com>
2020-01-19 14:35:37 +0800

    登录表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,15 +2,83 @@
 <!--    登录组件-->
   <div class="container">
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <van-cell-group>
+      <van-field
+        v-model.trim="loginForm.mobile"
+        required
+        :error-message="errorMessage.mobile"
+        label="手机号"
+        placeholder="请输入手机号"
+        @blur="checkMobile"
+      />
+      <van-field
+        v-model.trim="loginForm.code"
+        required
+        :error-message="errorMessage.code"
+        label="短信验证码"
+        placeholder="请输入短信验证码"
+        @blur="checkCode"
+      >
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div style="padding: 0 10px;margin-top: 20px">
+      <van-button type="primary" size="small" block style="border-radius: 20px" @click="login">登录</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  name: 'index',
+  data () {
+    return {
+      loginForm: {
+        mobile: null,
+        code: null
+      },
+      errorMessage: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    checkMobile () {
+      if (!this.loginForm.mobile) {
+        this.errorMessage.mobile = '手机号不能为空'
+        return false
+      }
+      if (!/^1[3-9]\d{9}$/.test(this.loginForm.mobile)) {
+        this.errorMessage.mobile = '手机号码格式不正确'
+        return false
+      }
+      this.errorMessage.mobile = ''
+      return true
+    },
+    checkCode () {
+      if (!this.loginForm.code) {
+        this.errorMessage.code = '请输入验证码'
+        return false
+      }
+      if (!/^\d{6}$/.test(this.loginForm.code)) {
+        this.errorMessage.code = '验证码是6位数字'
+        return false
+      }
+      this.errorMessage.code = ''
+      return true
+    },
+    async login () {
+      if (this.checkMobile() && this.checkCode()) {
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .van-cell-group {
+    margin-top: 20px;
+  }
 
 </style>
