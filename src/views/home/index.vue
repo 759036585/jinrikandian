ab574496670c9998730926d477a358be28b22edf
unknown <759036585@qq.com>
2020-01-19 16:26:48 +0800

    首页布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,91 @@
 <template>
 <!--    首页组件-->
-  <div class="container">首页组件</div>
+  <div class="container">
+    <van-tabs v-model="active" swipeable>
+      <van-tab :title="'标签'+ item" v-for="item in 10" :key="item">
+        <div class="scroll-wrapper">
+          <van-cell-group>
+            <van-cell
+              v-for="obj in 20"
+              :key="obj"
+              :title="item"
+            />
+          </van-cell-group>
+        </div>
+      </van-tab>
+    </van-tabs>
+    <span class="bar_btn">
+      <van-icon name="wap-nav"/>
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  name: 'index',
+  data () {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .van-tabs {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    /deep/ .van-tabs__wrap {
+      height: 36px;
+      padding-right: 36px;
+      .van-tab {
+        line-height: 36px;
+      }
+      .van-tab__line {
+        background-color: #3296fa;
+        height: 2px;
+      }
+    }
+    /deep/ .van-tabs__content {
+      flex: 1;
+      overflow: hidden;
+    }
+    /deep/ .van-tab__pane {
+      height: 100%;
+      .scroll-wrapper {
+        height: 100%;
+        overflow-y: auto;
+      }
+    }
+  }
+  .bar_btn {
+    width: 36px;
+    height: 36px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    &::before {
+      content: '';
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      z-index: 999;
+      box-shadow: 0 0 10px #999;
+      transform: scale(1,0.6);
+    }
+    .van-icon-wap-nav {
+      width: 100%;
+      height: 100%;
+      background: #fff;
+      text-align: center;
+      line-height: 35px;
+      position: relative;
+      z-index: 1000;
+      &::before {
+        font-size: 20px;
+      }
+    }
+  }
 
 </style>
