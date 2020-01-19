a653e7d8fae235fd5994003850794767ee50ecb9
unknown <759036585@qq.com>
2020-01-19 16:44:52 +0800

    初步实现上拉数据加载

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="scroll-wrapper">
-      <van-list v-model="upLoading" :finished="finished" @load="onLoad"></van-list>
+      <van-list v-model="upLoading" :finished="finished" @load="onLoad">
+        <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
+      </van-list>
     </div>
 </template>
 
@@ -10,12 +12,22 @@ export default {
   data () {
     return {
       upLoading: false, // 是否加载数据
-      finished: false // 加载是否完成
+      finished: false, // 加载是否完成
+      articles: []
     }
   },
   methods: {
     onLoad () {
-      console.log('开始加载数据')
+      // console.log('开始加载数据')
+      setTimeout(() => {
+        if (this.articles.length === 50) {
+          this.finished = true
+        } else {
+          let arr = Array.from(Array(10), (value, index) => index + this.articles.length + 1)
+          this.articles.push(...arr)
+          this.upLoading = false
+        }
+      }, 1000)
     }
   }
 }
