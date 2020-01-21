8a4dedbb7d2d8e79d80ed7082c7832c7f8bd67af
unknown <759036585@qq.com>
2020-01-21 12:04:00 +0800

    数据循环生成举报类型并渲染

--- src/views/home/components/more-action.vue
+++ src/views/home/components/more-action.vue
@@ -7,24 +7,20 @@
       </van-cell-group>
       <van-cell-group v-else>
         <van-cell icon="arrow-left" @click="isReport=false">返回</van-cell>
-        <van-cell>侵权</van-cell>
-        <van-cell>色情</van-cell>
-        <van-cell>暴力</van-cell>
-        <van-cell>低俗</van-cell>
-        <van-cell>不适</van-cell>
-        <van-cell>错误</van-cell>
-        <van-cell>其它</van-cell>
+        <van-cell v-for="item in reports" :key="item.value" >{{ item.label}}</van-cell>
       </van-cell-group>
     </div>
 </template>
 
 <script>
+import { reports } from '../../../api/constants'
 
 export default {
   name: 'more-action',
   data () {
     return {
-      isReport: false
+      isReport: false,
+      reports
     }
   }
 }
